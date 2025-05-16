deea8b807c3f7ed8905a86c69ca3f8b3705c5b25
huijingj <vanessajin0426@gmail.com>
2025-05-16 10:46:11 -0700

    5th part - working on deployment process

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -10,9 +11,9 @@ import connectToMongoDB from "./db/connectToMongoDB.js";
 
 import { app, server } from "./socket/socket.js";
 
-const PORT = process.env.PORT || 5000;
-
 dotenv.config();
+const __dirname = path.resolve();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
@@ -21,10 +22,11 @@ app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 app.use("/api/users", userRoutes)
 
-// app.get("/", (req, res) => {
-//     // root route http://localhost:5000/
-//     res.send("Hello World!");
-// });
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+})
 
 server.listen(PORT, () => {
     connectToMongoDB();
